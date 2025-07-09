85e2dba5aea96aaa60eb6e5e2cd2e108ffc6877e
olamiposi <olamiposi@soiposervices.com>
2025-07-09 00:55:47 -0300

    using http

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -21,7 +21,7 @@ class PredictionController extends Controller
         // Forward the image to the Python FastAPI server
         $response = Http::attach(
             'file', file_get_contents($image->getRealPath()), $image->getClientOriginalName()
-        )->post('http://persuasive.research.cs.dal.ca/skinvista/predict');
+        )->post('http://127.0.0.1:9123/predict');
 
         // Check if the request was successful
         if ($response->successful()) {
