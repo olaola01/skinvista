1881481050dbc987c2d5d895232a270af3b39f9d
olamiposi <olamiposi@soiposervices.com>
2025-07-09 01:19:48 -0300

    using localhost

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -21,7 +21,7 @@ class PredictionController extends Controller
         // Forward the image to the Python FastAPI server
         $response = Http::attach(
             'file', file_get_contents($image->getRealPath()), $image->getClientOriginalName()
-        )->post('http://127.0.0.1:9123/predict');
+        )->post('http://127.0.0.1:8000/predict');
 
         // Check if the request was successful
         if ($response->successful()) {
